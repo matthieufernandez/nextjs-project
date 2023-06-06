583f4dcbbd229df4946e8823809aed73833ec1b1
matt <mrmatthieufernandez@gmail.com>
2023-06-06 12:48:10 -0400

    added loading and 404 pages

--- src/app/hello/page.tsx
+++ src/app/hello/page.tsx
@@ -1,3 +1,13 @@
+"use client"
+
+import { useEffect } from "react";
+
 export default function Page() {
+
+    
+    // await new Promise((resolve) => setTimeout(resolve, 1000));
+
+    useEffect(() => {console.log("you used a useEffect hook")}, [])
+
     return <div>Hello world</div>
 }
